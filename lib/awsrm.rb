357724fe2dc2466d80674ae149371941bb509eab
k1LoW <k1lowxb@gmail.com>
2017-06-01 20:19:11 +0900

    bonsai dir

--- lib/awsrm.rb
+++ lib/awsrm.rb
@@ -1,10 +1,10 @@
 require 'awsrm/error'
 require 'awsrm/resource'
-require 'awsrm/autoscaling_group'
-require 'awsrm/ec2'
-require 'awsrm/route_table'
-require 'awsrm/subnet'
-require 'awsrm/vpc'
+require 'awsrm/resources/autoscaling_group'
+require 'awsrm/resources/ec2'
+require 'awsrm/resources/route_table'
+require 'awsrm/resources/subnet'
+require 'awsrm/resources/vpc'
 require 'awsrm/version'
 
 module Awsrm
