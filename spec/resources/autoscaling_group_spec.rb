4e86accbe50fc2de85d470e740c34aabbc93c66f
k1LoW <k1lowxb@gmail.com>
2017-08-02 16:02:33 +0900

    Fight to rubocop

--- spec/resources/autoscaling_group_spec.rb
+++ spec/resources/autoscaling_group_spec.rb
@@ -12,7 +12,9 @@ RSpec.describe Awsrm::AutoscalingGroup do
   it '#filters' do
     expect(Awsrm::AutoscalingGroup.filters(name: 'my-auto-scaling-group')).to eq ['my-auto-scaling-group']
     expect(Awsrm::AutoscalingGroup.filters(id: 'my-auto-scaling-group')).to eq ['my-auto-scaling-group']
+    # rubocop:disable Metrics/LineLength
     expect(Awsrm::AutoscalingGroup.filters(arn: 'arn:aws:autoscaling:::autoScalingGroup::autoScalingGroupName/my-auto-scaling-group')).to eq ['my-auto-scaling-group']
+    # rubocop:enable Metrics/LineLength
     expect(Awsrm::AutoscalingGroup.filters(
              tags: { Dept: 'Research', Role: 'WebServer' })).to eq ['my-auto-scaling-group']
     expect(Awsrm::AutoscalingGroup.filters(
