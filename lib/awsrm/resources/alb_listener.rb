3de5e4f32ec685cc1bf5c869f933aeb5e8eb7c59
k1LoW <k1lowxb@gmail.com>
2017-08-07 18:49:08 +0900

    Fix #14

--- lib/awsrm/resources/alb_listener.rb
+++ lib/awsrm/resources/alb_listener.rb
@@ -3,22 +3,25 @@ module Awsrm
     FILTER_MAP = {
       id: 'listener_arn',
       arn: 'listener_arn',
-      alb: ->(listener, value) { listener.load_balancer_arn == Awsrm::Alb.one(name: value).load_balancer_arn }
+      alb: ->(value) { { load_balancer_arn: Awsrm::Alb.one(name: value).load_balancer_arn } }
     }.freeze
 
     class << self
       def all(params)
-        listeners = elbv2_client.describe_listeners.map do |responce|
-          responce.listeners
+        listeners = params.map do |key, value|
+          raise UndefinedFilterParamError, key unless self::FILTER_MAP.key?(key)
+          if self::FILTER_MAP[key].is_a?(Proc)
+            next elbv2_client.describe_listeners(self::FILTER_MAP[key].call(value)).map do |responce|
+              responce.listeners
+            end.flatten
+          end
+          elbv2_client.describe_listeners(listener_arns: [value]).map do |responce|
+            responce.listeners
+          end.flatten
         end.flatten
         listeners.map do |listener|
-          ret = params.all? do |key, value|
-            raise UndefinedFilterParamError, key unless self::FILTER_MAP.key?(key)
-            next self::FILTER_MAP[key].call(listener, value) if self::FILTER_MAP[key].is_a?(Proc)
-            listener[self::FILTER_MAP[key]] == value
-          end
-          AlbListenerReader.new(listener) if ret
-        end.compact
+          AlbListenerReader.new(listener)
+        end
       end
 
       def filters(params) end
